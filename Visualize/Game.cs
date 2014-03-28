996800d97bb6530fbc9c2a7e40c37f8c972be00c
WouterLok <wouterlok300@gmail.com>
2014-03-28 15:34:59 +0100

    New interface

--- Visualize/Game.cs
+++ Visualize/Game.cs
@@ -6,6 +6,7 @@ using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Windows.Forms;
 using CG_2IV05.Common;
 using OpenTK;
 using OpenTK.Graphics;
@@ -14,16 +15,17 @@ using OpenTK.Input;
 using micfort.GHL.Math2;
 using micfort.GHL.Collections;
 using micfort.GHL.Serialization;
+using MouseEventArgs = System.Windows.Forms.MouseEventArgs;
 
 namespace CG_2IV05.Visualize
 {
-	class Game
+    public class Game
 	{
-		private GameWindow game;
-		private Vector3 CameraPos;
+        public HyperPoint<float> currentPosition = new HyperPoint<float>(0,0,0); 
+		public Vector3 CameraPos = Vector3.Zero;
 		private Vector2 Mouse;
 		private Vector2? LastMousePos;
-	    private Vector3 CurDirection = Vector3.Zero;
+	    private Vector3 CurDirection = Vector3.UnitX;
 		private Matrix4 lookAtMatrix;
 		private int texture;
 		private float speed = 10f;
@@ -31,37 +33,25 @@ namespace CG_2IV05.Visualize
 		private List<NodeWithData> releaseNodes; 
 
 		private Tree tree;
-		private NodeManager manager;
+		public NodeManager manager;
 		private Settings settingsForm;
 
 		private VBO vbo;
 	    private SkyBox skybox;
+	    private bool mouseControl = false;
 
-		public Game()
-		{
-			
-		}
+        private List<Keys> pressedKeys = new List<Keys>();
 
-		public void Run()
-		{
-			using(game = new GameWindow())
-			{
-				game.Load += game_Load;
-				game.Resize += game_Resize;
-				game.UpdateFrame += game_UpdateFrame;
-				game.RenderFrame += game_RenderFrame;
-				game.Unload += game_Unload;
+	    public Game()
+	    {
+	    }
 
-				game.Run();
-			}
-		}
-
-		void game_Unload(object sender, EventArgs e)
+		public void game_Unload()
 		{
 			manager.Stop();
 		}
 
-		void game_Load(object sender, EventArgs e)
+        public void game_Load()
 		{
 			CameraPos = new Vector3(0, 0, 10f);
 			Mouse = new Vector2(0, 0);
@@ -85,17 +75,13 @@ namespace CG_2IV05.Visualize
 			manager.VBOList = vbos;
 			manager.ReleaseNodes = releaseNodes;
 			manager.Position = CameraPos.ToHyperPoint();
-			manager.Start();
-
-			settingsForm = new Settings();
-			settingsForm.Show();
-			settingsForm.manager = manager;
+            manager.Start();
 
             skybox = new SkyBox(this);
             skybox.Load();
 
 			// setup settings, load textures, sounds
-			game.VSync = VSyncMode.On;
+			//game.VSync = VSyncMode.On;
 
 			using (FileStream file = File.OpenRead("Texture.fw.png"))
 			{
@@ -123,7 +109,7 @@ namespace CG_2IV05.Visualize
 			GL.Color3(Color.Blue);
 		}
 
-		void game_RenderFrame(object sender, FrameEventArgs e)
+        public void game_RenderFrame()
 		{
 			// render graphics
 			GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
@@ -161,114 +147,17 @@ namespace CG_2IV05.Visualize
 				releaseNodes.Clear();
 			}
 			
-			game.SwapBuffers();
 		}
 
-		void game_UpdateFrame(object sender, FrameEventArgs e)
-		{
-			#region mouse
-			if (LastMousePos == null || !(game.Mouse[MouseButton.Left] || game.Mouse[MouseButton.Right]))
-			{
-				LastMousePos = new Vector2(game.Mouse.X, game.Mouse.Y);
-			}
-
-			if (game.Mouse[MouseButton.Left] || game.Mouse[MouseButton.Right])
-			{
-				Vector2 mouseDiff = (new Vector2(game.Mouse.X, game.Mouse.Y) - LastMousePos.Value) * (1 / 50f);
-				Mouse = Mouse + mouseDiff;
-
-				Mouse.Y = MathHelper.Clamp(Mouse.Y, -MathHelper.PiOver2 + float.Epsilon, MathHelper.PiOver2 - float.Epsilon);
-				LastMousePos = new Vector2(game.Mouse.X, game.Mouse.Y);
-			}
-			#endregion
-			
-			#region CurDirection calculation
-			Matrix4 Rotation = Matrix4.CreateRotationY(Mouse.Y) * Matrix4.CreateRotationZ(-Mouse.X);
-			CurDirection = Vector3.Transform(Vector3.UnitX, Rotation);
-			#endregion
-
-			#region keyboard
-			Vector3 SideStep = Vector3.Cross(CurDirection, Vector3.UnitZ);
-
-			// add game logic, input handling
-			if (game.Keyboard[Key.Escape])
-			{
-				game.Exit();
-			}
-			if (game.Keyboard[Key.Up] || game.Keyboard[Key.W])
-			{
-				this.CameraPos = this.CameraPos + CurDirection*speed;
-			}
-			if (game.Keyboard[Key.Down] || game.Keyboard[Key.S])
-			{
-				this.CameraPos = this.CameraPos - CurDirection * speed;
-			}
-			if (game.Keyboard[Key.Left] || game.Keyboard[Key.A])
-			{
-				this.CameraPos = this.CameraPos - SideStep * speed;
-			}
-			if (game.Keyboard[Key.Right] || game.Keyboard[Key.D])
-			{
-				this.CameraPos = this.CameraPos + SideStep * speed;
-			}
-			if(game.Keyboard[Key.Number1])
-			{
-				speed = 0.1f;
-			}
-			if (game.Keyboard[Key.Number2])
-			{
-				speed = 0.2f;
-			}
-			if (game.Keyboard[Key.Number3])
-			{
-				speed = 0.5f;
-			}
-			if (game.Keyboard[Key.Number4])
-			{
-				speed = 1.0f;
-			}
-			if (game.Keyboard[Key.Number5])
-			{
-				speed = 2.0f;
-			}
-			if (game.Keyboard[Key.Number6])
-			{
-				speed = 3.0f;
-			}
-			if (game.Keyboard[Key.Number7])
-			{
-				speed = 5.0f;
-			}
-			if (game.Keyboard[Key.Number8])
-			{
-				speed = 10.0f;
-			}
-			if (game.Keyboard[Key.Number9])
-			{
-				speed = 20.0f;
-			}
-			if (game.Keyboard[Key.Number0])
-			{
-				speed = 50.0f;
-			}
-			#endregion
-
-			if (game.Mouse[MouseButton.Left])
-			{
-				this.CameraPos.Z = 1.75f;
-			}
-
-			manager.Position = CameraPos.ToHyperPoint();
-			lookAtMatrix = Matrix4.LookAt(this.CameraPos, this.CameraPos + CurDirection, Vector3.UnitZ);
-		}
-
-		void game_Resize(object sender, EventArgs e)
-		{
-			GL.Viewport(0, 0, game.Width, game.Height);
+	    public void game_Resize(object sender, EventArgs e)
+        {
+            GLControl control = (GLControl) sender;
+            
+			GL.Viewport(0, 0, control.Size.Width, control.Size.Height);
 
 			GL.MatrixMode(MatrixMode.Projection);
 			GL.LoadIdentity();
-			Matrix4 projMatrix = Matrix4.CreatePerspectiveFieldOfView(MathHelper.PiOver4, Convert.ToSingle(game.Width) / game.Height, 0.1f, 10000000f);
+			Matrix4 projMatrix = Matrix4.CreatePerspectiveFieldOfView(MathHelper.PiOver4, Convert.ToSingle(control.Size.Width) / control.Size.Height, 0.1f, 10000000f);
 			GL.LoadMatrix(ref projMatrix);
 		}
 
@@ -301,5 +190,130 @@ namespace CG_2IV05.Visualize
         {
             return CurDirection;
         }
+
+	    public void OnKeyDown(object sender, KeyEventArgs e)
+	    {
+            if (!pressedKeys.Contains(e.KeyCode))
+                pressedKeys.Add(e.KeyCode);
+	        processPressedKeys();
+	    }
+
+        public void OnKeyUp(object sender, KeyEventArgs e)
+        {
+            pressedKeys.Remove(e.KeyCode);
+            processPressedKeys();
+        }
+
+	    public void processPressedKeys()
+        {
+            Vector3 SideStep = Vector3.Cross(CurDirection, Vector3.UnitZ);
+
+            if (pressedKeys.Contains(Keys.Up) || pressedKeys.Contains(Keys.W))
+            {
+                this.CameraPos = this.CameraPos + CurDirection * speed;
+            }
+            if (pressedKeys.Contains(Keys.Down) || pressedKeys.Contains(Keys.S))
+            {
+                this.CameraPos = this.CameraPos - CurDirection * speed;
+            }
+            if (pressedKeys.Contains(Keys.Left) || pressedKeys.Contains(Keys.A))
+            {
+                this.CameraPos = this.CameraPos - SideStep * speed;
+            }
+            if (pressedKeys.Contains(Keys.Right) || pressedKeys.Contains(Keys.D))
+            {
+                this.CameraPos = this.CameraPos + SideStep * speed;
+            }
+            if (pressedKeys.Contains(Keys.D1))
+            {
+                speed = 0.1f;
+            }
+            if (pressedKeys.Contains(Keys.D2))
+            {
+                speed = 0.2f;
+            }
+            if (pressedKeys.Contains(Keys.D3))
+            {
+                speed = 0.5f;
+            }
+            if (pressedKeys.Contains(Keys.D4))
+            {
+                speed = 1.0f;
+            }
+            if (pressedKeys.Contains(Keys.D5))
+            {
+                speed = 2.0f;
+            }
+            if (pressedKeys.Contains(Keys.D6))
+            {
+                speed = 3.0f;
+            }
+            if (pressedKeys.Contains(Keys.D7))
+            {
+                speed = 5.0f;
+            }
+            if (pressedKeys.Contains(Keys.D8))
+            {
+                speed = 10.0f;
+            }
+            if (pressedKeys.Contains(Keys.D9))
+            {
+                speed = 20.0f;
+            }
+            if (pressedKeys.Contains(Keys.D0))
+            {
+                speed = 50.0f;
+            }
+            manager.Position = CameraPos.ToHyperPoint();
+            lookAtMatrix = Matrix4.LookAt(this.CameraPos, this.CameraPos + CurDirection, Vector3.UnitZ);
+        }
+
+	    public void OnMouseDown(object sender, MouseEventArgs e)
+	    {
+	       
+            if (e.Button == MouseButtons.Right)
+            {
+                this.mouseControl = true;
+            }
+            else if (e.Button == MouseButtons.Left)
+            {
+                this.CameraPos.Z = 1.75f;
+                manager.Position = CameraPos.ToHyperPoint();
+                lookAtMatrix = Matrix4.LookAt(this.CameraPos, this.CameraPos + CurDirection, Vector3.UnitZ);
+            }
+	    }
+        
+        public void OnMouseUp(object sender, MouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Right)
+            {
+                this.mouseControl = false;
+            }
+        }
+
+	    public void OnMouseMove(object sender, MouseEventArgs e)
+	    {
+            if (LastMousePos == null || !mouseControl)
+            {
+            	LastMousePos = new Vector2(e.Location.X, e.Location.Y);
+            }
+            if (mouseControl)
+            {
+                Vector2 mouseDiff = (new Vector2(e.Location.X, e.Location.Y) - LastMousePos.Value)*(1/50f);
+                Mouse = Mouse + mouseDiff;
+
+                Mouse.Y = MathHelper.Clamp(Mouse.Y, -MathHelper.PiOver2 + float.Epsilon,
+                                           MathHelper.PiOver2 - float.Epsilon);
+                LastMousePos = new Vector2(e.Location.X, e.Location.Y);
+            }
+
+	        #region CurDirection calculation
+            Matrix4 Rotation = Matrix4.CreateRotationY(Mouse.Y) * Matrix4.CreateRotationZ(-Mouse.X);
+            CurDirection = Vector3.Transform(Vector3.UnitX, Rotation);
+            #endregion
+            
+            manager.Position = CameraPos.ToHyperPoint();
+            lookAtMatrix = Matrix4.LookAt(this.CameraPos, this.CameraPos + CurDirection, Vector3.UnitZ);
+	    }
 	}
 }
